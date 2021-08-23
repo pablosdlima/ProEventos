6b1aa90bfc8255895a25a1d091de49e08474f92b
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2021-08-23 16:13:27 -0300

    Seção 5: angular, Interpolação, diretiva e Binding

--- Front/proeventos-App/src/app/app.module.ts
+++ Front/proeventos-App/src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; //importou a classe exportada no arquivo component.ts
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent, //modulos declarados. // modulo principal
     EventosComponent, //modulos declarados.
-    PalestrantesComponent //modulos declarados.
-  ],
+    PalestrantesComponent, //modulos declarados.
+    NavComponent
+   ],
   imports: [ //importações
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, //importação para comunicação de APIS
-    //BrowserAnimationsModule vai precisar
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/proeventos-App/src/app/eventos/eventos.component.ts
+++ Front/proeventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public eventos: any; // art declarado.
+  public eventos: any = []; // art declarado. Que quando chamado ao component, será pelo nome
+            //any possui elementos = any é um array
+            // isso permite que eventos possa vir vazio
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;    // larguraImagem
+  marginImg: number = 2;     // margemImagem
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';   
+
+  public get filtroLista() : string{ //retorno da prop privada
+      return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){ //retorno da prop privada
+      this._filtroLista = value;
+      this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+        (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { } //construtor recebe como parametro uma var chamada http que recebe Httpclient
 
@@ -15,10 +40,14 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
   public getEventos(): void { //metodo em angular.
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+    this.http.get('https://localhost:5001/api/eventos').subscribe( //pega dados da API controller / sql
+      response => this.eventos = response, // atr eventos recebe resposta da API acima.
+      error => console.log(error) //em caso de erro.
     );
   }
 }
